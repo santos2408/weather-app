c8fc28d33e6b2f538400ecefa70ba4e76170d050
Roger Santos <roger.santos36@gmail.com>
2022-05-25 12:21:58 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+const cityForm = document.querySelector('[data-js="change-location"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+const cityCard = document.querySelector(['[data-js="city-card"]'])
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
+let timeImage = document.querySelector('[data-js="time"]')
+
+// const getWeatherData = async inputValue => {
+//   const [{ Key, LocalizedName }] = await getCityData(inputValue)
+//   const [{ WeatherText, Temperature, WeatherIcon, IsDayTime }] = await getCityWeather(Key)
+//   // console.log([LocalizedName, WeatherText, Temperature, WeatherIcon, IsDayTime])
+//   return [LocalizedName, WeatherText, Temperature, WeatherIcon, IsDayTime]
+// }
+
+const getWeatherData = async inputValue => {
+  const [{ Key, LocalizedName }] = await getCityData(inputValue)
+  const [{ WeatherText, Temperature, WeatherIcon, IsDayTime }] = await getCityWeather(Key)
+  return { LocalizedName, WeatherText, Temperature, WeatherIcon, IsDayTime }
+}
+
+const showCityCard = () => {
+  const hasDisplayNoneClass = cityCard.classList.contains('d-none')
+  if (hasDisplayNoneClass) cityCard.classList.remove('d-none')
+}
+
+const insertWeatherInfoIntoDOM = (LocalizedName, WeatherText, Temperature) => {
+  cityNameContainer.textContent = LocalizedName
+  cityWeatherContainer.textContent = WeatherText
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
+
+const insertImagesIntoDOM = (IsDayTime, timeIcon) => {
+  IsDayTime ? timeImage.src = './src/day.svg' : timeImage.src = './src/night.svg'
+  timeIconContainer.innerHTML = timeIcon
+}
+
+const getWeatherInfo = async event => {
+  event.preventDefault()
+
+  const inputValue = event.target.city.value
+  const { 
+    LocalizedName,
+    WeatherText,
+    Temperature,
+    WeatherIcon, 
+    IsDayTime 
+  } = await getWeatherData(inputValue)
+  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+
+  showCityCard()
+  insertWeatherInfoIntoDOM(LocalizedName, WeatherText, Temperature)
+  insertImagesIntoDOM(IsDayTime, timeIcon)
+
+  cityForm.reset()
+}
+
+cityForm.addEventListener('submit', getWeatherInfo)
---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const APIKey = 'gp8tEyGvHKDGtwTDF3tCwbG0iMI1ZU1T'
+const baseUrl = `http://dataservice.accuweather.com/`
+
+const getCityUrl = cityName => 
+  `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getWeatherUrl = cityKey =>
+  `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
+
+const fetchData = async url => {
+  try {
+    const request = await fetch(url)
+
+    if (!request.ok) throw new Error ('Não foi possível obter os dados.')
+
+    return request.json()
+
+  } catch ({ name, message }) {
+    console.log(`${name}: ${message}`)
+  }
+}
+
+const getCityData = cityName => cityData = fetchData(getCityUrl(cityName))
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey))
